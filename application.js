e635d15feeca816dcb3ae73fc003facc9866beff
Stéphane <stephane.erbrech@gmail.com>
2015-04-19 20:16:44 +0200

    fix typo

--- application.js
+++ application.js
@@ -1,9 +1,11 @@
+(function() {
+  var $ = jQuery;
+  var resize_header = function () {
+    var h = $(window).height();
+    $("header-outer").height(h);
+  }
 
-function resize_header(){
-  var h = $(window).height();
-  $("header-outer").height(h);
-}
-
-$(function(){
-  resize_header();
-});
+  $(function(){
+    resize_header();
+  });
+})();
